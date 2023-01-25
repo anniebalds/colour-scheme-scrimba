0d3adecdf8477912290da65ef3e4c94db75e87e9
anniebalds <92921877+anniebalds@users.noreply.github.com>
2023-01-25 12:35:44 +0100

    css animation

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-const chooseColour = (e) => {
+const chooseColour = async (e) => {
     e.preventDefault()
     colourInput = document.getElementById('colour-pick').value
     modePick = document.getElementById('mode-pick').value
-        fetch(`https://www.thecolorapi.com/scheme?hex=${colourInput.substring(1)}&mode=${modePick}`)
-    .then(response => response.json())
-    .then(data => {
-        colours.innerHTML = ``;
-        data.colors.forEach((colour) => {
-            console.log(colour.hex.value)
+    
+    const res = await fetch(`https://www.thecolorapi.com/scheme?hex=${colourInput.substring(1)}&mode=${modePick}`)
+    const data = await res.json()
+    colours.innerHTML = ``;
+    data.colors.forEach((colour) => {
             colours.innerHTML += `
             <div class="one-colour" style="border: 5px solid ${colour.hex.value}">
               <img class="colour-img" src="${colour.image.bare}">
               <h3>${colour.hex.value}</h3>
             </div>`
-        })
-    })}
+    })
+}
 
 document.getElementById('submit-btn').addEventListener('click', chooseColour)
 const colours = document.getElementById('colours')
 let colourInput = ''
+
+
+//USING .THEN (original)
+// const chooseColour = (e) => {
+//     e.preventDefault()
+//     colourInput = document.getElementById('colour-pick').value
+//     modePick = document.getElementById('mode-pick').value
+//         fetch(`https://www.thecolorapi.com/scheme?hex=${colourInput.substring(1)}&mode=${modePick}`)
+//     .then(response => response.json())
+//     .then(data => {
+//         colours.innerHTML = ``;
+//         data.colors.forEach((colour) => {
+//             console.log(colour.hex.value)
+//             colours.innerHTML += `
+//             <div class="one-colour" style="border: 5px solid ${colour.hex.value}">
+//               <img class="colour-img" src="${colour.image.bare}">
+//               <h3>${colour.hex.value}</h3>
+//             </div>`
+//         })
+//     })}
+
+// document.getElementById('submit-btn').addEventListener('click', chooseColour)
+// const colours = document.getElementById('colours')
+// let colourInput = ''
